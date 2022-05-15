f9fd9fa7a37ae4e034f6db27020523486b0e3799
Nyasa <73618884+NyasaLuharuka@users.noreply.github.com>
2022-05-15 15:59:52 +0000

    Added more detail

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 
 print("Hi there. How are you feeling?")
 
-feelingStuff = {'good': ['good', 'happy', 'estatic', 'amazing', 'awesome', 'hopeful', 'optimistic', 'rosy', 'sanguine', 'animated', 'chirpy', 'jaunty', 'lilting', 'lively', 'perky', 'sprightful', 'sprightly', 'vivacious', 'carefree', 'careless', 'cavalier', 'devil-may-care', 'easygoing', 'happy-go-lucky', 'insouciant', 'lighthearted', 'unconcerned boon', 'gleeful', 'jocund', 'jolly', 'jovial', 'merry', 'mirthful', 'blissful', 'delighted', 'glad', 'gratified', 'happy', 'joyful', 'joyous', 'pleased', 'satisfied', 'tickled', 'beaming', 'grinning', 'laughing', 'smiling', 'great', 'perfect', 'grateful'], 'bad': ['sorrowful', 'ugly', 'awful','unhappy', 'miserable', 'mournful', 'upset', 'depressed', 'heartbroken', 'somber', 'sombre', 'blue', 'dejected', 'demoralised', 'demoralized', 'desolate', 'despondent', 'disconsolate', 'disheartened', 'dispirited', 'distressed', 'down', 'forlorn', 'glum', 'hurt', 'melancholy', 'pessimistic', 'sullen', 'troubled', 'wistful', 'bereaved', 'bitter', 'cheerless', 'doleful', 'downcast', 'down in dumps', 'down in mouth', 'gloomy', 'grief-stricken', 'grieved', 'hapless', 'heartsick', 'heavyhearted', 'in doldrums', 'in grief', 'in the dumps', 'joyless', 'low', 'low-spirited', 'lugubrious', 'moody', 'morbid', 'sad', 'bad', 'horrible', 'hopeless', 'useless', 'crumpled'], 'neutral':['normal', 'fine', 'decent', ' ']}
+feelingStuff = {'good': ['good', 'happy', 'estatic', 'amazing', 'awesome', 'hopeful', 'optimistic', 'rosy', 'sanguine', 'animated', 'chirpy', 'jaunty', 'lilting', 'lively', 'perky', 'sprightful', 'sprightly', 'vivacious', 'carefree', 'careless', 'cavalier', 'devil-may-care', 'easygoing', 'happy-go-lucky', 'insouciant', 'lighthearted', 'unconcerned boon', 'gleeful', 'jocund', 'jolly', 'jovial', 'merry', 'mirthful', 'blissful', 'delighted', 'glad', 'gratified', 'happy', 'joyful', 'joyous', 'pleased', 'satisfied', 'tickled', 'beaming', 'grinning', 'laughing', 'smiling', 'great', 'perfect', 'grateful'], 'bad': ['sorrowful', 'ugly', 'awful','unhappy', 'miserable', 'mournful', 'upset', 'depressed', 'heartbroken', 'somber', 'sombre', 'blue', 'dejected', 'demoralised', 'demoralized', 'desolate', 'despondent', 'disconsolate', 'disheartened', 'dispirited', 'distressed', 'down', 'forlorn', 'glum', 'hurt', 'melancholy', 'pessimistic', 'sullen', 'troubled', 'wistful', 'bereaved', 'bitter', 'cheerless', 'doleful', 'downcast', 'down in dumps', 'down in mouth', 'gloomy', 'grief-stricken', 'grieved', 'hapless', 'heartsick', 'heavyhearted', 'in doldrums', 'in grief', 'in the dumps', 'joyless', 'low', 'low-spirited', 'lugubrious', 'moody', 'morbid', 'sad', 'bad', 'horrible', 'hopeless', 'useless', 'crumpled'], 'neutral':['normal', 'fine', 'decent'], 'names': ['Patrick', 'Kaelyn', 'Eman', 'Rohan', 'Roham', 'Chelsea', 'Dad', 'Mom']}
+
+
 
 def goodStuff():
     queryGood = input()
     if queryGood == "yes":
         queryGood = input("Alright. Tell me about it.\n")
+        queryGoodList = queryGood.split()
+        for l in range(0, len(queryGoodList)):
+            for m in range(0, len(feelingStuff['names'])):
+                if queryGoodList[l] == feelingStuff['names'][m]:
+                    print("Yes, I know " + feelingStuff['names'][m] + ". Want to talk about her/him?")
+                    queryGood = input()
 def badStuff():
-    print(" ")
+    queryBad = input()
+    if queryBad == "yes":
+        queryBad = input("Alright. Tell me about it.\n")
+        queryBadList = queryBad.split()
+        for n in range(0, len(queryBadList)):
+            for o in range(0, len(feelingStuff['names'])):
+                if queryBadList[n] == feelingStuff['names'][o]:
+                    print("Want to talk about " + feelingStuff['names'][o] + "?")
+                    queryBad = input()
+                    if queryBad == 'yes':
+                        queryBad = input("I'm here. Tell me about " + feelingStuff['names'][o] + ". \n")
 def neuStuff():
     print(" ")
 
